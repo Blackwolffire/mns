195a68e1d1e4b2297112b342e0edd24d3baa057b
sdouga_a <amin.sdouga@epita.fr>
2019-07-03 18:21:41 +0200

    [LEXER] funcions: eatUntil eatWord predicateWork buffin

--- inc/lexer.h
+++ inc/lexer.h
@@ -1,6 +1,8 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#define BUFF_SIZE 4096
+
 enum TOKEN
 {
     ANY,
@@ -25,13 +27,14 @@ struct lexer
 {
     int istty;
     int fd;
-    char buff[4096];
+    char buff[BUFF_SIZE];
     short len;
     short offset;
     char iseof;
 };
 
 char initLexer(struct lexer* lex, int fd);
-char* eatToken(TOKEN toktype);
+char* eatToken(struct lexer* lex, TOKEN toktype);
+//void buffin(struct lexer* lex, short toSave);
 
 #endif /* !LEXER_H */
--- src/lexer.c
+++ src/lexer.c
@@ -1,23 +1,93 @@
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "lexer.h"
 
 char initLexer(struct lexer* lex, int fd)
 {
     if (!lex || fd < 0)
-        return EXIT_FAILURE;
+        return -1;
 
     lex->istty = isatty(fd);
     lex->fd = fd;
     lex->len = 0;
     lex->offset = 0;
     lex->iseof = 0;
-    memset(lex->buff, 0, 4096);
+    memset(lex->buff, 0, BUFF_SIZE);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
 
-char* eatToken(TOKEN toktype)
+static void buffin(struct lexer* lex, short toSave)
 {
+    if (toSave > lex->len || toSave < 0)
+        return;
+    if (toSave)
+        memmove(lex->buff, lex->buff + len - toSave, toSave);
+    lex->len = toSave;
+    lex->offset = 0;
+    memset(lex->buff + toSave, 0, BUFF_SIZE - toSave);
+    lex->len += read(lex->fd, lex->buff + lex->len, BUFF_SIZE - lex->len);
+}
 
+static char predicatWord(char carac)
+{
+    return (carac != ' ' && carac != '\n' && carac != '$' && carac != '\''
+            && carac != '\"' && carac != ';' && carac != '?' && carac != '|'
+            && carac != '<' && carac != '>'  && carac != '(' && carac != ')'
+            && carac != '=');
+}
+
+static char eatWord(struct lexer* lex, short* i)
+{
+    do
+    {
+        if (lex->offset + *i >= lex->len)
+            buffin(lex, *i + 1);
+        if (lex->iseof)
+            return 1;
+        *i += 1;
+    } while (predicatWord(lex->buff[offset + *i]));
+    *i += 1;
+    return 0;
+}
+
+static char eatUntil(struct lexer* lex, char carac, short* i)
+{
+    do
+    {
+        if (lex->offset + *i >= lex->len)
+            buffin(lex, *i + 1);
+        if (lex->iseof)
+            return 1;
+        *i += 1;
+    } while (lex->buff[offset + *i] != carac);
+    *i += 1;
+    return 0;
+}
+
+char* eatToken(struct lexer* lex, TOKEN toktype)
+{
+    char* tok = NULL;
+    short i = 0;
+
+    if (!lex || lex->iseof)
+        return NULL;
+
+    if (lex->offset >= lex->len)
+        buffin(lex, 0);
+
+    switch (toktype)
+    {
+        case ANY:
+            eatWord(lex, &i);
+            break;
+        default:
+            return NULL;
+    }
+    if (!i)
+        return NULL;
+    tok = calloc(i, sizeof(char));
+    tok = strncpy(tok, lex->buff + lex->offset, i);
+    return tok;
 }
