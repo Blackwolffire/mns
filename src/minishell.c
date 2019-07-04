d9972c16f2254db402f418d2864541f2c9d0b7e3
sdouga_a <amin.sdouga@epita.fr>
2019-07-04 22:19:55 +0200

    [MNS] return the right value

--- src/minishell.c
+++ src/minishell.c
@@ -1,6 +1,9 @@
+#define _POSIX_C_SOURCE 200112L
+
 #include <err.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -27,6 +30,7 @@ int main(int argc, char** argv)
     }
     if (fdin < 0)
         err(127, "%s: No such file or directory", argv[1]); // watch memleak
+    setenv("?", "0", 1);
     initLexer(&lex, fdin);
     struct ast* tree = parse(&lex);
     execute(tree);
@@ -34,5 +38,5 @@ int main(int argc, char** argv)
     if (isopen)
         close(fdin);
 
-    return 0;
+    return strtol(getenv("?"), NULL, 10);
 }
